b181d823e093b1f97b94ac01f859ccbe13be53cb
Евгений Зверев <nrvnqsr_@outlook.com>
2023-11-01 13:02:50 +0400

    add: scene recommendation

---
+++ src/scenes/recomendation-making.scene.ts
@@ -0,0 +1,34 @@
+import { Ctx, Message, Wizard, WizardStep } from 'nestjs-telegraf';
+import { TelegramUserService } from 'src/telegram-user/telegram-user.service';
+import { Scenes } from 'telegraf';
+
+@Wizard('recomendation-making')
+export class RecomendationWizard {
+    constructor(private readonly telegramUser: TelegramUserService) {}
+    @WizardStep(1)
+    async onEnter(@Ctx() ctx: Scenes.WizardContext) {
+        ctx.reply(
+            'Ссылка или название аниме на сервисе, которым вы пользуетесь',
+        );
+        ctx.wizard.next();
+    }
+    @WizardStep(2)
+    async onAnime(
+        @Ctx() ctx: Scenes.WizardContext,
+        @Message() msg: { text: string },
+    ) {
+        if (!this.verifyExisting(msg.text))
+        {
+            ctx.reply
+            ctx.scene.reenter();
+        }
+            console.log("added")
+        else 
+        ctx.scene.leave();
+    }
+
+    private verifyExisting(anime: string): boolean {
+        if (anime) return true;
+        else return false;
+    }
+}
