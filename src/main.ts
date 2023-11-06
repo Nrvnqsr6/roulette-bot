ee726d434abd2909d5ecb0fd1e42932b68ff4365
Riniker <zverekqw12@gmail.com>
2023-11-06 14:39:34 +0400

    fix: clear session on start (temporary)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import fs from 'fs';
 
 async function bootstrap() {
+    fs.writeFile('user.json', '', (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log('Successfully wrote file');
+        }
+    });
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const port = configService.get('port');
