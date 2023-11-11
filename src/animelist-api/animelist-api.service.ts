22fe33994421291643ffb5b701f70519201aa095
Евгений Зверев <nrvnqsr_@outlook.com>
2023-11-11 18:22:51 +0400

    refactor: mal-api access

--- src/animelist-api/animelist-api.service.ts
+++ src/animelist-api/animelist-api.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { Animelist } from 'src/enums/animelists';
 import { iAnimeAPI } from 'src/interfaces/animelist-api';
 import { MalAPI } from './mal.api';
+import { TelegramUser } from 'src/telegram-user/entities/telegram-user.entity';
 
 @Injectable()
 export class AnimelistApiService {
     private apiWrapper: iAnimeAPI;
     // constructor();
-    GetUserAnimeList(userListID: number, userListSource: Animelist) {
-        this.setAnimeList(userListSource);
-        return this.apiWrapper.getUserAnimeList(userListID);
+    GetUserAnimeList(user: TelegramUser) {
+        this.setAnimeList(user.UserListSource);
+        return this.apiWrapper.getUserAnimeList(user.UserListID);
     }
 
     private setAnimeList(userListSource) {
--- src/animelist-api/mal.api.ts
+++ src/animelist-api/mal.api.ts
@@ -1,16 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { iAnimeAPI } from 'src/interfaces/animelist-api';
 
 export class MalAPI implements iAnimeAPI {
     private readonly url = 'http://api.myanimelist.net/v2';
     private readonly queryParams = {
-        limit: 500,
-        nsfw: true,
+        //limit: 500,
+        nsfw: false,
         status: 'completed',
     };
     private readonly headers = { 'X-MAL-CLIENT-ID': process.env.MAL_TOKEN };
 
-    async getUserAnimeList(username: string) {
+    async getUserAnimeList(
+        username: string,
+        status?: string,
+    ): Promise<AxiosResponse> {
         return axios.get(`${this.url}/users/${username}/animelist`, {
             params: this.queryParams,
             headers: this.headers,
